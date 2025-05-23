aea2e06e43df4750139e67cd4ad458a30cb78d28
somegoat <scrapegoatofficial@gmail.com>
2025-05-23 13:18:02 +0530

    small changes

--- src/actions/Search.js
+++ src/actions/Search.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Text } from '@botonic/react';
 
-export default async ({ i }) => {
-
-    console.log(i);
-    const query = i.data;
+export default async (ctx) => {
+    const query = ctx;
+    console.log(query);
     return <Text>We need to search for it but we lack the functionality at the moment!</Text>
-}
+};
